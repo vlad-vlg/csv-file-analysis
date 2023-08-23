af48b477431e8ccd8efbd48abbbd664f2aed4613
Влад Иванович <vlad@ya.ru>
2023-08-23 16:06:52 +0300

    Создание основы программы

---
+++ Scvanalyst.py
@@ -0,0 +1,11 @@
+# Программа анализа .scv файлов
+
+import tkinter as tk
+
+#Создание главного окна
+window = tk.Tk()
+window.geometry('550x550')
+window.title('Программа анализа .scv файлов')
+
+# Запуск цикла mainloop
+window.mainloop()
