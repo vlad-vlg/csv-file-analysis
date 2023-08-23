e1d6db31f72be391f52e9fc8749afe57395c099d
Влад Иванович <vlad@ya.ru>
2023-08-23 16:19:10 +0300

    Добавление полей вывода

--- Scvanalyst.py
+++ Scvanalyst.py
@@ -7,5 +7,24 @@ window = tk.Tk()
 window.geometry('550x550')
 window.title('Программа анализа .scv файлов')
 
+# Создание меток вывода
+label_00 = tk.Label(text = 'Файл:')
+label_00.grid(row=0, column=0, padx=10, pady=10, sticky='e')
+
+label_01 = tk.Label(text = '')
+label_01.grid(row=0, column=1, sticky='w')
+
+label_10 = tk.Label(text = 'Строк:')
+label_10.grid(row=1, column=0, padx=10, pady=10, sticky='e')
+
+label_11 = tk.Label(text = '')
+label_11.grid(row=1, column=1, sticky='w')
+
+label_20 = tk.Label(text = 'Столбцов:')
+label_20.grid(row=2, column=0, padx=10, pady=10, sticky='e')
+
+label_21 = tk.Label(text = '')
+label_21.grid(row=2, column=1, sticky='w')
+
 # Запуск цикла mainloop
 window.mainloop()
