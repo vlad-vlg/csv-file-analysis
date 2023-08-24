923c009bbc1dee8645ae6b4ac74c83a05f6fac77
Влад Иванович <vlad@ya.ru>
2023-08-24 16:55:52 +0300

    Изменение оформления полей

--- CsvAnalyst.py
+++ CsvAnalyst.py
@@ -49,31 +49,28 @@ window.title('Программа анализа .csv файлов')
 label_00 = tk.Label(text = 'Файл:')
 label_00.grid(row=0, column=0, padx=10, pady=10, sticky='e')
 
-label_01 = tk.Label(text = '', relief='sunken', bg='azure', width=50, bd=2)
+label_01 = tk.Label(window, text = '', relief='sunken', bg='azure', bd=3)
 label_01.grid(row=0, column=1, padx=10, pady=10, sticky='ew')
 
 label_10 = tk.Label(text = 'Строк:')
 label_10.grid(row=1, column=0, padx=10, pady=10, sticky='e')
 
-label_11 = tk.Label(text = '', relief='sunken', bg='azure', width=50, bd=2)
+label_11 = tk.Label(text = '', relief='sunken', bg='azure', bd=3)
 label_11.grid(row=1, column=1, padx=10, pady=10, sticky='ew')
 
 label_20 = tk.Label(text = 'Столбцов:')
 label_20.grid(row=2, column=0, padx=10, pady=10, sticky='e')
 
-label_21 = tk.Label(text = '', relief='sunken', bg='azure', width=50, bd=2)
+label_21 = tk.Label(text = '', relief='sunken', bg='azure', bd=3)
 label_21.grid(row=2, column=1, padx=10, pady=10, sticky='ew')
 
 # Создание текстового вывода с прокруткой
-output_text = st(height=22, width=50, bd=5)
+output_text = st(height=22, width=50, bd=5,  selectforeground='snow', selectbackground='blue4')
 output_text.grid(row=3, column=1, padx=10, pady=10, sticky='w')
 
 # Создание кнопки
 button = tk.Button(window, text='Прочитать файл', command=process_button)
 button.grid(row=4, column=1)
-
-
-
 
 # Запуск цикла mainloop
 window.mainloop()
